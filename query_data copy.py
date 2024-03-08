6d2011c4e2f436d49f67ffaf45ad07fbe959e8a0
Akshaya Bhalikha DS <65649398+AKSHAYA-162@users.noreply.github.com>
2024-03-08 08:50:36 -0500

    Update query_data copy.py

--- query_data copy.py
+++ query_data copy.py
@@ -26,24 +26,27 @@ Answer the question based on the above context: {question}
 def main(query_text):
     os.environ["OPENAI_API_KEY"] = "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"
 
-    # Prepare the DB.
+    # To Prepare the database
     embedding_function = OpenAIEmbeddings()
-    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+    database = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
-    # Search the DB.
-    results = db.similarity_search_with_relevance_scores(query_text, k=3)
-    if len(results) == 0 or results[0][1] < 0.7:
-        print(f"Unable to find matching results.")
+    # To search the Database
+    result = database.similarity_search_with_relevance_scores(query_text, k=3)
+    if len(result) == 0 or result[0][1] < 0.7:
+        print(f"I'm unable to find the answer to your Question, try another Question.")
         return
 
-    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    #Template of the content
+    context_text = "\n\n\n---\n\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
     print(prompt)
 
+    #creating a model for open AI
     model = ChatOpenAI()
     response_text = model.predict(prompt)
 
+    #the document source
     sources = [doc.metadata.get("source", None) for doc, _score in results]
     formatted_response = {"response": response_text, "sources": sources}
     # print(formatted_response)
@@ -51,29 +54,33 @@ def main(query_text):
 
 
 
-
+#from the "https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps#build-a-chatgpt-like-app" website
+#
+#
 st.title("ChatGPT-like clone")
 
-# Set OpenAI API key from Streamlit secrets
+# Set OpenAI API key from Streamlit secrets(toml file)
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-# Set a default model
+# Setting a default model - gpt-3.5-turbo
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-# Initialize chat history
+# For initilization chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
+# Display chat messages from history on the app
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Accept user input
+# This step helps accepting the user input
 if prompt := st.chat_input("What is up?"):
+    
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
+    
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
